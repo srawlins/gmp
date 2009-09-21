e4c850db73b80fdfbf24444e45fe7d75a517a397
Sam Rawlins <sam.rawlins@gmail.com>
2009-09-21 10:31:37 -0700

    Bump gemp to 0.1.3, rearrange some tests

---
+++ test/tc_zerodivisionexceptions.rb
@@ -0,0 +1,17 @@
+class TC_ZeroDivisionExceptions < Test::Unit::TestCase
+  def setup
+    @a = GMP::Z.new(10)
+    @b = GMP::Z.new()
+    @c = GMP::Q.new(1)
+    @d = GMP::Q.new()
+  end
+  
+  def test_division_by_zero
+    assert_raise(ZeroDivisionError) { @a.tdiv(0) }
+    assert_raise(ZeroDivisionError) { @a.tdiv(@b) }
+    assert_raise(ZeroDivisionError) { @d.inv }
+    assert_raise(ZeroDivisionError) { @d.inv! }
+    assert_raise(ZeroDivisionError) { @c/0 }
+    assert_raise(ZeroDivisionError) { @c/@d }
+  end
+end
--- test/unit_tests.rb
+++ test/unit_tests.rb
@@ -8,25 +8,8 @@ require 'tc_q'
 require 'tc_cmp'
 require 'tc_q_basic'
 require 'tc_z_exponentiation'
+require 'tc_zerodivisionexceptions'
 
-
-class TC_ZeroDivisionExceptions < Test::Unit::TestCase
-  def setup
-    @a = GMP::Z.new(10)
-    @b = GMP::Z.new()
-    @c = GMP::Q.new(1)
-    @d = GMP::Q.new()
-  end
-  
-  def test_division_by_zero
-    assert_raise(ZeroDivisionError) { @a.tdiv(0) }
-    assert_raise(ZeroDivisionError) { @a.tdiv(@b) }
-    assert_raise(ZeroDivisionError) { @d.inv }
-    assert_raise(ZeroDivisionError) { @d.inv! }
-    assert_raise(ZeroDivisionError) { @c/0 }
-    assert_raise(ZeroDivisionError) { @c/@d }
-  end
-end
 
 class TC_sgn_neg_abs < Test::Unit::TestCase
   def setup
