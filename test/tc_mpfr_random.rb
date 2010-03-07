b275a373118b88adf02ea3de142da2cb3edd6a5f
Sam Rawlins <sam.rawlins@gmail.com>
2010-03-07 12:29:09 -0700

    Added more mpfr_urandomb tests

--- test/tc_mpfr_random.rb
+++ test/tc_mpfr_random.rb
@@ -17,33 +17,33 @@ class TC_MPFR_Random < Test::Unit::TestCase
     end
   end
   
-  # def test_reseed
-    # @c = GMP::RandState.new
-    # @c.seed(1000)
-    # assert_equal( 6461, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(42961, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(44699, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # @c.seed(1000)
-    # assert_equal( 6461, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(42961, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(44699, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # @c.seed(1000)
-    # assert_equal( 6461, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(42961, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-    # assert_equal(44699, @c.urandomb(16), "GMP::RandState should re-seed correctly.")
-  # end
+  def test_reseed
+    @c = GMP::RandState.new
+    @c.seed(1000)
+    assert_in_delta(GMP::F("0.78522728792921048"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.94422653925308231"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.60678541956109799"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    @c.seed(1000)
+    assert_in_delta(GMP::F("0.78522728792921048"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.94422653925308231"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.60678541956109799"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    @c.seed(1000)
+    assert_in_delta(GMP::F("0.78522728792921048"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.94422653925308231"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+    assert_in_delta(GMP::F("0.60678541956109799"), @c.mpfr_urandomb, 1e-12, "GMP::RandState should re-seed correctly.")
+  end
   
-  # def test_random_independent_states
-    # @d = GMP::RandState.new
-    # @d.seed(13579)
-    # @e = GMP::RandState.new
-    # @e.seed(13579)
+  def test_random_independent_states
+    @d = GMP::RandState.new
+    @d.seed(577)
+    @e = GMP::RandState.new
+    @e.seed(577)
     
-    # assert_equal( 392, @d.urandomb(12), "GMP::RandState should be independent correctly.")
-    # assert_equal( 507, @d.urandomb(12), "GMP::RandState should be independent correctly.")
-    # assert_equal( 392, @e.urandomb(12), "GMP::RandState should be independent correctly.")
-    # assert_equal( 507, @e.urandomb(12), "GMP::RandState should be independent correctly.")
-    # assert_equal(1657, @d.urandomb(12), "GMP::RandState should be independent correctly.")
-    # assert_equal(1657, @e.urandomb(12), "GMP::RandState should be independent correctly.")
-  # end
+    assert_in_delta(GMP::F("0.39810885576093713"), @d.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+    assert_in_delta(GMP::F("0.97212443610368071"), @d.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+    assert_in_delta(GMP::F("0.39810885576093713"), @e.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+    assert_in_delta(GMP::F("0.97212443610368071"), @e.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+    assert_in_delta(GMP::F("0.23084385480845748"), @d.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+    assert_in_delta(GMP::F("0.23084385480845748"), @e.mpfr_urandomb, 1e-12, "GMP::RandState should be independent correctly.")
+  end
 end
