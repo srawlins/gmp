109fc55e10f830c341cdfd5b37e24d79f6575012
srawlins <sam.rawlins@gmail.com>
2010-09-11 11:17:51 -0700

    Another fix to EVERYONE

--- ext/EVERYONE-macbook-os-x-10.6.rb
+++ ext/EVERYONE-macbook-os-x-10.6.rb
@@ -5,16 +5,19 @@
 puts "In case you were wondering, this ruby is #{RUBY_VERSION}"
 sleep 1
 
-["", "--no-mpfr"].each do |no_mpfr|
-  %w(1.8.7 1.9.1 1.9.2).each do |ruby_v|
-    ["5.0.1 3.0.0", "4.3.2 2.4.2"].each do |gmp_mpfr|
-      system("./ccmt.sh #{no_mpfr} #{ruby_v} #{gmp_mpfr} | " +
+#["", "--no-mpfr"].each do |no_mpfr|
+["--no-mpfr"].each do |no_mpfr|
+  #%w(1.8.7 1.9.1 1.9.2).each do |ruby_v|
+  %w(1.9.2).each do |ruby_v|
+    #["5.0.1 3.0.0", "4.3.2 2.4.2"].each do |gmp_mpfr|
+    ["4.3.2 2.4.2"].each do |gmp_mpfr|
+      system("./ccmt.sh #{no_mpfr} #{ruby_v} #{gmp_mpfr} 2>&1 | " +
              "tee ../results-test-#{ruby_v}-" +
              "#{no_mpfr!='' ? gmp_mpfr.sub(/ .*/, '') : gmp_mpfr.sub(/ /, '-')}")
       if ARGV[0] == "--benchmark"
         Dir.chdir('../benchmark')
 
-        system("rvm use #{ruby_v}; LIBS=-lgmp ./runbench -n | " +
+        system("source ~/.rvm/scripts/rvm; rvm use #{ruby_v}; LIBS=-lgmp ./runbench -n 2>&1 | " +
                "tee ../results-benchmark-#{ruby_v}-" +
                "#{no_mpfr!='' ? gmp_mpfr.sub(/ .*/, '') : gmp_mpfr.sub(/ /, '-')}")
         Dir.chdir('../ext')
