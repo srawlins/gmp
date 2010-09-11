b2644e887d2ecdc61ef59bc4b779903351b3a9e1
Sam Rawlins <sam@jesus.(none)>
2010-09-11 14:36:37 -0700

    Added srb.sh, extended EVERYONE-eeepc

--- ext/EVERYONE-eeepc-linux-2.6.32-ubuntu.rb
+++ ext/EVERYONE-eeepc-linux-2.6.32-ubuntu.rb
@@ -5,16 +5,28 @@
 puts "In case you were wondering, this ruby is #{RUBY_VERSION}"
 sleep 1
 
-["", "--no-mpfr"].each do |no_mpfr|
+benchmark = false
+mpfr = ["", "--no-mpfr"]
+
+until ARGV.empty?
+  case
+  when ARGV[0]=="--benchmark" then benchmark=true; ARGV.shift
+  when ARGV[0]=~/--mpfr=(.*)/
+    mpfr=$1.split(','); ARGV.shift
+  else ARGV.shift
+  end
+end
+
+mpfr.each do |no_mpfr|
   %w(1.8.7 1.9.1 1.9.2).each do |ruby_v|
-    ["5.0.1 3.0.0", "4.3.2 2.4.2"].each do |gmp_mpfr|
+    ["5.0.1 3.0.0", "4.3.2 2.4.2", "4.3.1 2.4.2"].each do |gmp_mpfr|
       system("./ccmt.sh #{no_mpfr} #{ruby_v} #{gmp_mpfr} 2>&1 | " +
              "tee ../results-test-#{ruby_v}-" +
              "#{no_mpfr!='' ? gmp_mpfr.sub(/ .*/, '') : gmp_mpfr.sub(/ /, '-')}")
-      if ARGV[0] == "--benchmark"
+      if benchmark
         Dir.chdir('../benchmark')
 
-        system("source ~/.rvm/scripts/rvm; rvm use #{ruby_v}; LIBS=-lgmp ./runbench -n 2>&1 | " +
+        system("./srb.sh #{ruby_v} 2>&1 | " +
                "tee ../results-benchmark-#{ruby_v}-" +
                "#{no_mpfr!='' ? gmp_mpfr.sub(/ .*/, '') : gmp_mpfr.sub(/ /, '-')}")
         Dir.chdir('../ext')
