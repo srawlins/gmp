3be64a51ccd8fc07a7dce0f29cffd91619fe5d4c
Sam Rawlins <sam.rawlins@gmail.com>
2012-09-10 23:01:56 -0700

    Added a Rakefile!!! And some to GMP::F#to_s tests

---
+++ Rakefile
@@ -0,0 +1,61 @@
+# Dependencies base directory. I have to figure out how to... not hard code this?
+DEPENDENCIES_DIR = '/usr/local'
+
+task :clean do
+  sh "cd ext && make clean"
+end
+
+task :extconf => [:clean] do
+  sh "cd ext && ruby extconf.rb #{gmp_opt} #{mpfr_opt}"
+end
+
+task :make => [:extconf] do
+  sh "cd ext && make"
+end
+
+task :test => [:make] do
+  sh "cd test && ruby unit_tests.rb"
+end
+
+task :report => [:test] do
+  puts "RUBY:   " + `ruby -v`
+  puts "GMP:    " + `ruby -r './lib/gmp' -e "puts GMP::GMP_VERSION"`
+  puts "GMP_CC: " + `ruby -r './lib/gmp' -e "puts GMP::GMP_CC"`
+  if ENV['MPFR'] != 'no-mpfr'
+    puts "MPFR:   " + `ruby -r './lib/gmp' -e "puts GMP::MPFR_VERSION"`
+  else
+    puts "MPFR:   --no-mpfr"
+  end
+end
+
+namespace :dependencies do
+  task :list do
+    suffix = (DEPENDENCIES_DIR.size+1)..-1
+    puts "GMP packages installed into #{DEPENDENCIES_DIR}:"
+    Dir.glob(File.join(DEPENDENCIES_DIR, 'gmp') + '*').each do |dir|
+      puts dir[suffix]
+    end
+    puts ""
+
+    puts "MPFR packages installed #{DEPENDENCIES_DIR}:"
+    Dir.glob(File.join(DEPENDENCIES_DIR, 'mpfr') + '*').each do |dir|
+      puts dir[suffix]
+    end
+  end
+end
+
+def gmp_opt
+  version = ENV['GMP'] || '5.0.5'
+  '--with-gmp-dir='+File.join(DEPENDENCIES_DIR, "gmp-#{version}")
+end
+
+def mpfr_opt
+  version = ENV['MPFR'] || '3.1.1'
+  if version == 'no-mpfr'
+    return '--no-mpfr'
+  else
+    return '--with-mpfr-dir='+File.join(DEPENDENCIES_DIR, "mpfr-#{version}")
+  end
+end
+
+
--- test/tc_f_to_s.rb
+++ test/tc_f_to_s.rb
@@ -2,10 +2,15 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
 
 class TC_F_to_s < Test::Unit::TestCase
   def setup
-    @rs = GMP::RandState.new(11213)
+    @initial_default_prec = GMP::F.default_prec
   end
 
-  def test_to_s
+  def teardown
+    GMP::F.default_prec = @initial_default_prec
+  end
+
+  def test_to_s_default_prec
+    rs = GMP::RandState.new(11213)
     if GMP.const_defined? :MPFR_VERSION
       strings = [
         "0.25352409199307085e+0",     "0.63701645188411673e+1",     "0.11775590017886098e+0",
@@ -25,9 +30,29 @@ class TC_F_to_s < Test::Unit::TestCase
     # 2^100 - 1. The next two are distributed between 0 and 2^110 - 1. The last
     # are distributed between 0 and 2^190 - 1.
     (0..8).each do |i|
-      n = GMP::F(@rs.urandomb(10 * (i+10)), 53)
-      d = GMP::F(@rs.urandomb(10 * (i+10)), 53)
+      n = GMP::F(rs.urandomb(10 * (i+10)), 53)
+      d = GMP::F(rs.urandomb(10 * (i+10)), 53)
       assert_equal(strings[i], (n/d).to_s, "GMP::F#to_s should be correct for default precision numbers.")
+    end
+  end
+
+  def test_to_s_bigger_prec
+    rs  = GMP::RandState.new(19937)
+    rs2 = GMP::RandState.new(21701)
+    if GMP.const_defined? :MPFR_VERSION
+      strings = [
+        "0.655393663692186710105e-2",               "0.36430999484273987746204e+4",                "0.11976788674896651459533244e+3",
+        "0.2606381328092210487064345210e+0",        "0.977167229464647196661643216645e-2",         "0.755006431167835276749654244022956e-1",
+        "0.41292361235767736087211972667683449e+1", "0.41857769284263224368201967544187408820e-2", "0.5145285970595521572673546620320724197645e-3"
+      ]
+    else # todo
+    end
+
+    (0..8).each do |i|
+      GMP::F.default_prec = 64+(8*i)
+      n = GMP::F(rs.urandomb(144+rs2.urandomb(4).to_i))
+      d = GMP::F(rs.urandomb(144+rs2.urandomb(4).to_i))
+      assert_equal(strings[i], (n/d).to_s, "GMP::F#to_s should be correct for different precision numbers.")
     end
   end
 end
