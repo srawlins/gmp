21358bac8b203cf586702e438e520f6032cb24ab
Sam Rawlins <sam.rawlins@gmail.com>
2012-12-08 23:27:59 -0700

    Ugly fix for non-1.9.3 regex :/

--- lib/gmp.rb
+++ lib/gmp.rb
@@ -8,20 +8,23 @@ ENV['PATH'] = [File.expand_path(
 
 require File.dirname(__FILE__) + '/../ext/gmp'
 
-module GMP
-  def self.sprintf(format, *args)
-    first_pct = format.index '%'
-    result = format[0...first_pct]
-    format.gsub(/(?<!%)%[0#+ ']*[0-9]*.?[0-9]*[a-zA-Z][^%]*/) do |fragment|
-      arg = args.shift
-      if fragment =~ /%[0#+ ']*[0-9]*.?[0-9]*[ZQF]/
-        result << sprintf2(fragment, arg)
-      elsif fragment =~ /%[0#+ ']*[0-9]*.?[0-9]*[PR]/ && GMP.const_defined?(:MPFR_VERSION)
-        result << GMP::F.sprintf2(fragment, arg)
-      else
-        result << (fragment % arg)
+unless RUBY_VERSION =~ /^1.8/
+  module GMP
+    def self.sprintf(format, *args)
+      first_pct = format.index '%'
+      result = format[0...first_pct]
+      #format.gsub(/(?<!%)%[0#+ ']*[0-9]*.?[0-9]*[a-zA-Z][^%]*/) do |fragment|
+      format.gsub(Regexp.new('(?<!%)%[0#+ \']*[0-9]*.?[0-9]*[a-zA-Z][^%]*')) do |fragment|
+        arg = args.shift
+        if fragment =~ /%[0#+ ']*[0-9]*.?[0-9]*[ZQF]/
+          result << sprintf2(fragment, arg)
+        elsif fragment =~ /%[0#+ ']*[0-9]*.?[0-9]*[PR]/ && GMP.const_defined?(:MPFR_VERSION)
+          result << GMP::F.sprintf2(fragment, arg)
+        else
+          result << (fragment % arg)
+        end
       end
+      result
     end
-    result
   end
 end
