25f921a1e1de2f6d91d346fee11899958545c389
Sam Rawlins <sam.rawlins@gmail.com>
2014-01-23 11:27:25 -0800

    Changing spacing style in all of mprnd.c

--- ext/mprnd.c
+++ ext/mprnd.c
@@ -11,8 +11,8 @@ VALUE r_mprndsg_new(int argc, VALUE *argv, VALUE klass)
   (void)klass;
   res_value = 0;
 
-  mprnd_make_struct(res, res_value);
-  rb_obj_call_init(res, argc, argv);
+  mprnd_make_struct (res, res_value);
+  rb_obj_call_init (res, argc, argv);
   return res;
 }
 
@@ -27,68 +27,68 @@ VALUE r_mprnd_initialize(int argc, VALUE *argv, VALUE self)
     prefix = "GMP";
   else
     prefix = "MPFR";
-  switch (FIX2INT(mode)) {
+  switch (FIX2INT (mode)) {
   case 0:
-    sprintf(name_val, "%s_RNDN", prefix);
-    ieee754 = rb_str_new2("roundTiesToEven");
+    sprintf (name_val, "%s_RNDN", prefix);
+    ieee754 = rb_str_new2 ("roundTiesToEven");
     break;
   case 1:
-    sprintf(name_val, "%s_RNDZ", prefix);
-    ieee754 = rb_str_new2("roundTowardZero");
+    sprintf (name_val, "%s_RNDZ", prefix);
+    ieee754 = rb_str_new2 ("roundTowardZero");
     break;
   case 2:
-    sprintf(name_val, "%s_RNDU", prefix);
-    ieee754 = rb_str_new2("roundTowardPositive");
+    sprintf (name_val, "%s_RNDU", prefix);
+    ieee754 = rb_str_new2 ("roundTowardPositive");
     break;
   case 3:
-    sprintf(name_val, "%s_RNDD", prefix);
-    ieee754 = rb_str_new2("roundTowardNegative");
+    sprintf (name_val, "%s_RNDD", prefix);
+    ieee754 = rb_str_new2 ("roundTowardNegative");
     break;
   case 4:
-    sprintf(name_val, "%s_RNDA", prefix);
-    ieee754 = rb_str_new2("roundAwayFromZero");
+    sprintf (name_val, "%s_RNDA", prefix);
+    ieee754 = rb_str_new2 ("roundAwayFromZero");
     break;
   default:
-    sprintf(name_val, "%s_RNDN", prefix);
-    ieee754 = rb_str_new2("roundTiesToEven");
+    sprintf (name_val, "%s_RNDN", prefix);
+    ieee754 = rb_str_new2 ("roundTiesToEven");
   }
-  name = rb_str_new2(name_val);
-  rb_iv_set(self, "@mode",    mode);
-  rb_iv_set(self, "@name", name);
-  rb_iv_set(self, "@ieee754", ieee754);
+  name = rb_str_new2 (name_val);
+  rb_iv_set (self, "@mode",    mode);
+  rb_iv_set (self, "@name", name);
+  rb_iv_set (self, "@ieee754", ieee754);
   return Qnil;
 }
 
 VALUE r_mprnd_inspect(VALUE self)
 {
-  return rb_iv_get(self, "@name");
+  return rb_iv_get (self, "@name");
 }
 
 void init_gmprnd()
 {
-  ID new_id = rb_intern("new");
-  mGMP = rb_define_module("GMP");
+  ID new_id = rb_intern ("new");
+  mGMP = rb_define_module ("GMP");
 
-  cGMP_Rnd = rb_define_class_under(mGMP, "Rnd", rb_cObject);
+  cGMP_Rnd = rb_define_class_under (mGMP, "Rnd", rb_cObject);
 
-  rb_define_singleton_method(cGMP_Rnd, "new", r_mprndsg_new, -1);
-  rb_define_method(cGMP_Rnd, "initialize", r_mprnd_initialize, -1);
-  rb_define_method(cGMP_Rnd, "inspect", r_mprnd_inspect, 0);
+  rb_define_singleton_method (cGMP_Rnd, "new", r_mprndsg_new, -1);
+  rb_define_method (cGMP_Rnd, "initialize", r_mprnd_initialize, -1);
+  rb_define_method (cGMP_Rnd, "inspect", r_mprnd_inspect, 0);
 
   rb_define_attr (cGMP_Rnd, "mode",    1, 0);
   rb_define_attr (cGMP_Rnd, "name",    1, 0);
   rb_define_attr (cGMP_Rnd, "ieee754", 1, 0);
 
-  rb_define_const(mGMP, "GMP_RNDN", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(0)));
-  rb_define_const(mGMP, "GMP_RNDZ", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(1)));
-  rb_define_const(mGMP, "GMP_RNDU", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(2)));
-  rb_define_const(mGMP, "GMP_RNDD", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(3)));
+  rb_define_const (mGMP, "GMP_RNDN", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (0)));
+  rb_define_const (mGMP, "GMP_RNDZ", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (1)));
+  rb_define_const (mGMP, "GMP_RNDU", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (2)));
+  rb_define_const (mGMP, "GMP_RNDD", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (3)));
   /* MPFR 3.0.0 */
-  rb_define_const(mGMP, "MPFR_RNDN", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(0)));
-  rb_define_const(mGMP, "MPFR_RNDZ", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(1)));
-  rb_define_const(mGMP, "MPFR_RNDU", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(2)));
-  rb_define_const(mGMP, "MPFR_RNDD", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(3)));
-  rb_define_const(mGMP, "MPFR_RNDA", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX(4)));
+  rb_define_const (mGMP, "MPFR_RNDN", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (0)));
+  rb_define_const (mGMP, "MPFR_RNDZ", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (1)));
+  rb_define_const (mGMP, "MPFR_RNDU", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (2)));
+  rb_define_const (mGMP, "MPFR_RNDD", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (3)));
+  rb_define_const (mGMP, "MPFR_RNDA", rb_funcall (cGMP_Rnd, new_id, 1, INT2FIX (4)));
   /* end MPFR 3.0.0 */
 }
 
